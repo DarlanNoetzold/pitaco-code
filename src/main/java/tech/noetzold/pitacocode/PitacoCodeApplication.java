4d110a9cb8848d9a93f6a3ecebc2986da0aa29e9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-07-29 08:12:05 -0300

    aa

--- src/main/java/tech/noetzold/pitacocode/PitacoCodeApplication.java
+++ src/main/java/tech/noetzold/pitacocode/PitacoCodeApplication.java
@@ -2,12 +2,21 @@ package tech.noetzold.pitacocode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PitacoCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PitacoCodeApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
 	}
 
 }
--- src/main/java/tech/noetzold/pitacocode/controller/PostController.java
+++ src/main/java/tech/noetzold/pitacocode/controller/PostController.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/alerta")
+@RequestMapping("/post")
 public class PostController {
 
     @Autowired
