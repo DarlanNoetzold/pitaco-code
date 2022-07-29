00747905a493281d80f927e8c7faef4e4c03e49d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-07-29 07:43:11 -0300

    aa

--- src/main/java/tech/noetzold/pitacocode/controller/PostController.java
+++ src/main/java/tech/noetzold/pitacocode/controller/PostController.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.pitacocode.model.Post;
 import tech.noetzold.pitacocode.service.PostService;
-import tech.noetzold.pitacocode.service.AnswerService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,22 +23,19 @@ import java.util.List;
 public class PostController {
 
     @Autowired
-    PostService alertaService;
-
-    @Autowired
-    AnswerService imagemService;
+    PostService postService;
 
     @Transactional
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<Page<Post>> getAll(HttpServletRequest request, HttpServletResponse response, Pageable pageable) {
-        return new ResponseEntity<>(alertaService.findAll(pageable), HttpStatus.OK);
+        return new ResponseEntity<>(postService.findAll(pageable), HttpStatus.OK);
     }
 
     @Transactional
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Post> getAlertaId(@PathVariable("id") long id) {
 
-        Post post = alertaService.findById(id).get();
+        Post post = postService.findById(id).get();
 
         return new ResponseEntity<Post>(post, HttpStatus.OK);
     }
@@ -48,7 +44,7 @@ public class PostController {
     @RequestMapping(value = "/usuario/{usuario}", method = RequestMethod.GET)
     public ResponseEntity<Collection<Post>> getPostUsuario(@PathVariable("usuario") String usuario) {
 
-        List<Post> posts = alertaService.findAllByUsuario(usuario);
+        List<Post> posts = postService.findAllByUsuario(usuario);
 
         return new ResponseEntity<Collection<Post>>(posts, HttpStatus.OK);
     }
@@ -58,7 +54,7 @@ public class PostController {
     public ResponseEntity<Post> save(@RequestBody Post post, HttpServletRequest request, HttpServletResponse response) {
         try {
             post.setData_cadastro(Calendar.getInstance());
-            post = alertaService.save(post);
+            post = postService.save(post);
             return new ResponseEntity<Post>(post, HttpStatus.CREATED);
         } catch (Exception e) {
             e.printStackTrace();
@@ -67,7 +63,7 @@ public class PostController {
     }
     @GetMapping("remover/{id}")
     public String remover(@PathVariable("id") Long id) {
-        alertaService.deleteById(id);
+        postService.deleteById(id);
         return "redirect:/home";
     }
 
