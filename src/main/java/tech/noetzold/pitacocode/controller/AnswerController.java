45e3ec8aa7782058e22476cb1424b515577331f5
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-08-03 13:25:25 -0300

    fix bug

--- src/main/java/tech/noetzold/pitacocode/controller/AnswerController.java
+++ src/main/java/tech/noetzold/pitacocode/controller/AnswerController.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.pitacocode.model.Answer;
 import tech.noetzold.pitacocode.service.AnswerService;
+import tech.noetzold.pitacocode.service.PostService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,19 +19,22 @@ import java.util.Collection;
 @RequestMapping("/answer")
 public class AnswerController {
     @Autowired
-    AnswerService imagemService;
+    AnswerService answerService;
+
+    @Autowired
+    PostService postService;
 
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
     @Transactional
     public ResponseEntity<Collection<Answer>> getAll(HttpServletRequest request, HttpServletResponse response) {
-        return new ResponseEntity<Collection<Answer>>(imagemService.findAll(), HttpStatus.OK);
+        return new ResponseEntity<Collection<Answer>>(answerService.findAll(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
     @Transactional
     public ResponseEntity<Answer> getImagemById(@PathVariable("id") long id) {
 
-        Answer answer = imagemService.findById(id).get();
+        Answer answer = answerService.findById(id).get();
 
         return new ResponseEntity<Answer>(answer, HttpStatus.OK);
     }
@@ -39,7 +43,8 @@ public class AnswerController {
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public ResponseEntity<Answer> save(@RequestBody Answer answer) {
         try {
-            answer = imagemService.save(answer);
+            answer.setPost(postService.findById(answer.getPost().getId()).get());
+            answer = answerService.save(answer);
             return new ResponseEntity<Answer>(answer, HttpStatus.CREATED);
         } catch (Exception e) {
             e.printStackTrace();
